107deb34903b40c151a37d395693e08c4aaf9bc1
Andreh Marques <andrehmarquez@gmail.com>
2025-03-12 09:50:01 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("S");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("A");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("N");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("T");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("O");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.Write("S");
+
+
+
+Console.ResetColor();
